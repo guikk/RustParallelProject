3ddf768ea8685ffb140a31347fc036d9015b5e80
Guilherme Klein Kern <guilherme.klein99@gmail.com>
2022-11-13 21:15:50 +0100

    Add beanchmarks

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,46 @@ const VSIZE: usize = 10_000_000;
 const INTMIN: i32 = -1000;
 const INTMAX: i32 = 1000;
 
+fn benchmark() {
+    let mut rng = rand::thread_rng();
+    const N_SAMPLES: usize = 100;
+    println!("size,s1,s2,p1,p2");
+
+    for size in vec![1_000_000, 5_000_000, 10_000_000, 15_000_000, 25_000_000] {
+
+        let s1: f64 = 0.0; //std::iter::repeat_with(|| {
+        //     let nums: Vec<i32> = (0..size).map(|_| rng.gen_range(INTMIN..INTMAX)).collect();
+        //     let start = std::time::Instant::now();
+        //     let m4 = max_product::sort_maximum_product(nums.to_owned());
+        //     start.elapsed().as_secs_f64() * 1000.0
+        // }).take(N_SAMPLES).sum::<f64>() / N_SAMPLES as f64;
+
+        let s2: f64 = std::iter::repeat_with(|| {
+            let nums: Vec<i32> = (0..size).map(|_| rng.gen_range(INTMIN..INTMAX)).collect();
+            let start = std::time::Instant::now();
+            let m4 = max_product::sp_maximum_product(nums.to_owned());
+            start.elapsed().as_secs_f64() * 1000.0
+        }).take(N_SAMPLES).sum::<f64>() / N_SAMPLES as f64;
+
+        let p1: f64 = 0.0; //std::iter::repeat_with(|| {
+        //     let nums: Vec<i32> = (0..size).map(|_| rng.gen_range(INTMIN..INTMAX)).collect();
+        //     let start = std::time::Instant::now();
+        //     let m4 = max_product::par_sort_maximum_product(nums.to_owned());
+        //     start.elapsed().as_secs_f64() * 1000.0
+        // }).take(N_SAMPLES).sum::<f64>() / N_SAMPLES as f64;
+
+        let p2: f64 = std::iter::repeat_with(|| {
+            let nums: Vec<i32> = (0..size).map(|_| rng.gen_range(INTMIN..INTMAX)).collect();
+            let start = std::time::Instant::now();
+            let m4 = max_product::par_local_limits_maximum_product(nums.to_owned());
+            start.elapsed().as_secs_f64() * 1000.0
+        }).take(N_SAMPLES).sum::<f64>() / N_SAMPLES as f64;
+
+        println!("{:.4},{:.4},{:.4},{:.4},{:.4}", size, s1, s2, p1, p2);
+    }
+
+}
+
 fn make_svg() {
     let mut rng = rand::thread_rng();
     let nums: Vec<i32> = (0..VSIZE).map(|_| rng.gen_range(INTMIN..INTMAX)).collect();
@@ -69,5 +109,6 @@ fn single_comparison() {
 
 fn main() {
     single_comparison();
-    make_svg();
+    //make_svg();
+    //benchmark();
 }
